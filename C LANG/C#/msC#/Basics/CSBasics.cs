cf9abe70d06cb50cc308671a7a94a6a8e9e9f2fb
PatrickArlan <patrickarlanbrequillo@gmail.com>
2026-04-27 15:54:54 +0800

    feat: initialize C# project structure with basic configuration and build tasks

---
+++ C LANG/C#/msC#/Basics/CSBasics.cs
@@ -0,0 +1,11 @@
+//This code consists of print, string literal, variables, chars
+
+namespace MainProgram.Basics;
+
+public class Lesson01
+{
+    public static void Run()
+    {
+        Console.WriteLine(3.14F);
+    }
+}
---
+++ C LANG/C#/msC#/Program.cs
@@ -0,0 +1,11 @@
+﻿using MainProgram.Basics;
+
+namespace MainProgram;
+
+internal class Program
+{
+    static void Main()
+    {
+        Lesson01.Run();
+    }
+}
