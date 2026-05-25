c4a32cd7e0e95e8a042b7729478efb6f4e5b33d7
PatrickArlan <patrickarlanbrequillo@gmail.com>
2026-05-25 23:03:40 +0800

    docs: completed lesson 3.2 flexbox - finished all flexbox activities - built responsive hris card layout - answered the remaining quesitions

--- HRIS/HRIS-PAT/backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ HRIS/HRIS-PAT/backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87299e3fb07bf4c81da78b20fafa8853a92bab92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+234cd7a517f5af2021d8701f0aefc5d4b458c1c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
